e1aa6bbd1febdb9eead161c8e8e0a4d09591c0b0
mesut.cakir <mesut.cakir@argelog.com.tr>
2016-09-07 01:11:29 +0300

    Proje arama, kategorileme alt yapısı için sayfa işlevleri eklendi.

--- Config/WebConfig.js
+++ Config/WebConfig.js
@@ -1,38 +1,60 @@
 ﻿angular.module('AngularBlog',["ngSanitize",  'ngRoute'])
 .config(['$routeProvider', function ($routeProvider) {
   $routeProvider
-    .when("/", {templateUrl: "partials/pages/blog.html", controller: "BlogController", caseInsensitiveMatch: true })
+  .when("/", {templateUrl: "partials/pages/blog.html", controller: "BlogController", caseInsensitiveMatch: true })
+  .when("/:category", {templateUrl: "partials/pages/blog.html", controller: "BlogController", caseInsensitiveMatch: true })
+    .when("/?seachString=:seachString", {templateUrl: "partials/pages/blog.html", controller: "BlogController", caseInsensitiveMatch: true })
     .when("/post/:id", {templateUrl: "partials/pages/blog_post.html", controller: "BlogPostController", caseInsensitiveMatch: true })
-      .when("/search", {templateUrl: "partials/pages/search.html", controller: "SearchController", caseInsensitiveMatch: true })
+      .when("/search/:seachString", {templateUrl: "partials/pages/search.html", controller: "SearchController", caseInsensitiveMatch: true })
     .when("/Contact", {templateUrl: "partials/pages/contact.html", controller: "PageController", caseInsensitiveMatch: true })
     .otherwise({templateUrl: "partials/pages/404.html", controller: "PageController", caseInsensitiveMatch: true });
 }])
-.controller("BlogController",["$scope","$http",
-function($scope,$http){
+.controller("BlogController",["$scope","$http","$routeParams","$location",
+function($scope,$http,$routeParams,$location){
+  console.log($routeParams)
+  $scope.SearchModel = {
+    SearchString:$routeParams.seachString,
+    Search: function(){
+$location.path('/search/'+  $scope.SearchModel.SearchString);
+    }
+  };
+
   $http.get("Config/Blog.json").success(function(data){
         $scope.Model = data;
     });
 }])
-.controller("BlogPostController",["$scope","$http","$routeParams",
-function($scope,$http,$routeParams){
+.controller("BlogPostController",["$scope","$http","$routeParams","$location",
+function($scope,$http,$routeParams,$location){
 
   $http.get("Config/Blog_Post_1.json",{
-    params:{
-    id:  $routeParams.id
-    }
+    params:$routeParams
   }).success(function(data){
         $scope.Model = data;
     });
 }])
-.controller("SearchController",["$scope","$http",
-function($scope,$http){
-  $http.get("Config/Search.json").success(function(data){
+.controller("SearchController",["$scope","$http","$routeParams","$location",
+function($scope,$http,$routeParams,$location){
+  console.log($routeParams);
+  $scope.SearchModel = {
+    SearchString:$routeParams.seachString,
+    Search: function(){
+$location.path('/search/'+  $scope.SearchModel.SearchString);
+    }
+  };
+  $http.get("Config/Search.json",{
+    params:$routeParams
+  }).success(function(data){
         $scope.Model = data;
     });
 }])
-.controller("PageController",["$scope","$http",
-function($scope,$http){
-
+.controller("PageController",["$scope","$http","$routeParams","$location",
+function($scope,$http,$routeParams,$location){
+  $scope.SearchModel = {
+    SearchString:$routeParams.seachString,
+    Search: function(){
+$location.path('/search/'+  $scope.SearchModel.SearchString);
+    }
+  };
 }])
 
 .controller("HeadController",["$scope","$http",
