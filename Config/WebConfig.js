55b263a1a5df723fa7ecbbcb917d0d237cf3f236
mesut.cakir <mesut.cakir@argelog.com.tr>
2016-09-06 15:14:23 +0300

    Blog temel çalışması başlatıldı.ç

---
+++ Config/WebConfig.js
@@ -0,0 +1,32 @@
+﻿angular.module('AngularBlog',["ngSanitize",  'ngRoute'])
+.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    .when("/", {templateUrl: "partials/pages/blog.html", controller: "BlogController", caseInsensitiveMatch: true })
+    .when("/post/1", {templateUrl: "partials/pages/blog_post.html", controller: "BlogController", caseInsensitiveMatch: true })
+    .when("/Contact", {templateUrl: "partials/pages/contact.html", controller: "PageController", caseInsensitiveMatch: true })
+    .otherwise({templateUrl: "partials/pages/404.html", controller: "PageController", caseInsensitiveMatch: true });
+}])
+.controller("BlogController",["$scope","$http",
+function($scope,$http){
+  $http.get("Config/Blog.json").success(function(data){
+        $scope.Model = data;
+    });
+}])
+.controller("PageController",["$scope","$http",
+function($scope,$http){
+
+}])
+
+.controller("HeadController",["$scope","$http",
+function($scope,$http){
+  $http.get("Config/Head.json").success(function(data){
+        $scope.Model = data;
+    });
+}])
+
+.controller("MenuController",["$scope","$http",
+function($scope,$http){
+  $http.get("Config/Menu.json").success(function(data){
+        $scope.Model = data;
+    });
+}]);
